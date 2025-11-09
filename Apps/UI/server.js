f855d7e2ed73775e88738ba7b8a6cca4dc6170d0
itsDavid08 <128751214+itsDavid08@users.noreply.github.com>
2025-11-09 22:07:30 +0000

    Feat: npm installation in UI

---
+++ Apps/UI/server.js
@@ -0,0 +1,18 @@
+// Apps/ui/server.js
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000; // A porta que o seu ui.yml e Dockerfile esperam
+
+// Serve ficheiros estáticos (como o seu index.html, css, js)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota principal que serve o seu HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor UI a correr em http://localhost:${PORT}`);
+});
